80dd8e35064ac2385631d6c838e819e9cf8ff75b
JoaoRodriguesqtw <joaopgsrodrigues@gmail.com>
2026-08-26 22:24:39 -0300

    Feat: criando primeiras rotas

--- minha-primeira-api/index.js
+++ minha-primeira-api/index.js
@@ -1,2 +1,13 @@
 import express from "express";
 
+const app = express(); //Primeiro pilar: instancia do express
+
+app.get("/", (req,res) => {
+    res.send("bananinha")
+});
+
+app.get("/livros", (req,res) => {
+    res.send("Hello world!")
+});
+
+app.listen(3000); // porta a ser ouvida
