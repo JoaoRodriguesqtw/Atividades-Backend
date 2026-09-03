09da45cf71eedc2c0f4a1764e62c6e6e9ac074e4
JoaoRodriguesqtw <joaopgsrodrigues@gmail.com>
2026-09-02 22:20:28 -0300

    docs: definição do que o projeto irá fazer

--- minha-primeira-api/index.js
+++ minha-primeira-api/index.js
@@ -2,12 +2,50 @@ import express from "express";
 
 const app = express(); //Primeiro pilar: instancia do express
 
+/*idlivro identificador/int
+dstitulo string
+dsautor  string
+fgdisponivel  boolean
+ */
+
+let livros = []; // banko of dados
+
+// metodos + caminhos + funcção
 app.get("/", (req,res) => {
-    res.send("bananinha")
+    res.send("bananilson farofa")
 });
 
 app.get("/livros", (req,res) => {
     res.send("Hello world!")
 });
 
+
 app.listen(3000); // porta a ser ouvida
+
+
+
+
+
+
+/*
+cadastrarm livros
+post
+
+buscar todos livros
+get
+
+buscar um livro pelo nome
+get
+
+buscar um livro pelo id
+get
+
+emprestar livros
+put/patch
+
+devolver livros
+put/patch
+
+deletar livros
+delete
+*/
